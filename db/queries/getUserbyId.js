f86b3abce631c9a532844183757a62844bab0a58
Paul Formby <paulformby@icloud.com>
2023-12-20 23:04:10 +0000

    create getUserbyId and getUserbyid-test

---
+++ db/queries/getUserbyId.js
@@ -0,0 +1,13 @@
+// Function to retrieve a user based on their ID using the buy/sell database
+const getUserById = function (id) {
+  return pool
+    .query("SELECT * FROM users WHERE id = $1 LIMIT 1", [id])
+    .then((result) => {
+      // IDs are unique, so we only expect one result
+      return result.rows[0];
+    })
+    .catch((err) => {
+      // Handling any errors that might occur during the database query
+      console.error(err.message);
+    });
+};
---
+++ test/getUserById-test.js
@@ -0,0 +1,45 @@
+const { assert } = require('chai');
+const { getUserById, pool } = require('./getUserById');
+
+describe('getUserById', () => {
+  // Mocking the pool object for testing purposes
+  const originalQuery = pool.query;
+  pool.query = () => {
+    return {
+      then: (callback) => callback({ rows: [{ id: 1, name: 'Test User' }] }),
+    };
+  };
+
+  it('should retrieve a user by their ID', () => {
+    const userId = 1;
+    return getUserById(userId).then(function(user) {
+      // Assert that the result is an object
+      assert(user && typeof user === 'object');
+      // Assert that the user ID matches the expected ID
+      assert(user.id === userId);
+      // Add more assertions based on your expected results
+    });
+  });
+
+  it('should handle errors during the database query', () => {
+    // Mocking an error during the query for testing error handling
+    pool.query = () => {
+      return {
+        then: (callback) => callback({ rows: [] }),
+      };
+    };
+
+    const userId = 1;
+    return getUserById(userId).then(function(user) {
+      // Add assertions for error handling
+      // Assert that the result is undefined (or whatever your error handling returns)
+      assert(user === undefined);
+      // Add more assertions based on your error handling logic
+    });
+  });
+
+  // Reset the pool.query to the original implementation after testing
+  after(() => {
+    pool.query = originalQuery;
+  });
+});
