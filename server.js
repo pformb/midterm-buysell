9f8822ecf8ca36fe8462a682d8ba1b8ac02d3322
Paul Formby <paulformby@icloud.com>
2023-12-27 08:12:19 +0000

    create browse page ejs, scss and route

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
 const loginRoutes = require('./routes/login');
 const registerRoutes = require('./routes/register');
+const browseRoutes = require('./routes/browse');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -42,6 +43,7 @@ app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 app.use('/login', loginRoutes);
 app.use('/register', registerRoutes);
+app.use('/browse', browseRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
