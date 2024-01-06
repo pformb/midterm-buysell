5c9919b5313212ad3610d811e608a2d2dd58bf2a
Kershia1 <insatiable.meerkat@gmail.com>
2024-01-06 21:51:59 +0000

    Adding to registration route to link to database.

--- routes/register.js
+++ routes/register.js
@@ -5,4 +5,27 @@ router.get('/', (req, res) => {
   res.render('register');
 });
 
+//Post route for register
+router.post('/', (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 12);
+
+  db.query(`
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+  `, [user.name, user.email, user.password])
+    .then((data) => {
+      const user = data.rows[0];
+      req.session.user_id = user.id;
+      res.redirect('/');
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
 module.exports = router;
+
