47697ef94fd6593bc9ef45f764152696e1fd865f
Kershia1 <insatiable.meerkat@gmail.com>
2024-01-08 16:53:34 +0000

    Add loginForm.js made, now successfully handles onclick event to login user

---
+++ public/scripts/loginForm.js
@@ -0,0 +1,35 @@
+//handles login-form submission and on-click event for login button
+
+$(() => {
+
+  const $loginForm = $(`
+  <div class="login-container">
+  <form id="login-form">
+
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required>
+
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" required>
+      <p>Forgot your Password?</p>
+      <button type="button" id="submit-btn">Login In Button</button>
+
+      <p>By logging in you agree to the Terms of Service and Privacy Policy</p>
+      <!-- Links not yet applied that Paul added in the Figma-->
+      <p>Don't have an account?  Sign Up Here</p>
+      <!--Make this a seperate div or footer?-->
+  </form>
+</div>
+`);
+  window.$loginForm = $loginForm;
+
+  $loginForm.on('submit', function (event) {
+    event.preventDefault();
+
+    const data = $(this).serialize();
+    login(data)
+      .then(function (json) {
+        header.update(json);
+      });
+  });
+});
