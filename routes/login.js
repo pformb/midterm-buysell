7e62d6882173168e7ac05863309d187431f8a966
Kershia1 <insatiable.meerkat@gmail.com>
2024-01-08 17:16:44 +0000

    FIX login logic and session-mgmt added to login route, now checks database for exisit user, redirects accordingly

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const { getUserById } = require('../db/queries/getUserByEmail');
+
 
 router.get('/', (req, res) => {
   res.render('login');
+});
+
+router.post('/', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await getUserByEmail(email);
+  try {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user_id = user.id;
+      res.redirect('/browse').status(200).send('Login Successful');
+    } else {
+      res.redirect('/login').status(401).send('Login Failed: invalid email or password');
+    }
+  } catch (err) {
+    res.status(500).send("An error occurred during the login process");
+  }
 });
 
 module.exports = router;
