a7495df3fcbdc4dbb28f9b330de5958502a3529d
Kershia1 <insatiable.meerkat@gmail.com>
2024-01-10 05:45:33 +0000

    ADD session.user_id to session.user to acess whole user object. App broken

--- routes/browse.js
+++ routes/browse.js
@@ -5,8 +5,9 @@ const db = require("../db/connection.js");
 
 router.get("/", (req, res) => {
   getAllProducts(db).then((data) => {
+    const user = req.session.user;
     console.log(data);
-    res.render("browse", {data});
+    res.render("browse", {data, user});
   });
 
 
--- routes/login.js
+++ routes/login.js
@@ -13,7 +13,7 @@ router.post('/', async (req, res) => {
   const user = await getUserByEmail(email);
   try {
     if (user && bcrypt.compareSync(password, user.password)) {
-      req.session.user_id = user.id;
+      req.session.user = user;
       res.redirect('/browse').status(200).send('Login Successful');
     } else {
       res.redirect('/login').status(401).send('Login Failed: invalid email or password');
