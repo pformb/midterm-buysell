b8dba57788bce564bfd18743d595a2e46ef37d84
Griego86 <dimi1086@hotmail.com>
2024-01-10 15:14:12 -0800

    Connected the db to /account via getUserByEmail

--- db/queries/getUserByEmail.js
+++ db/queries/getUserByEmail.js
@@ -1,13 +1,19 @@
 //changed '' to "". Please note will cause syntax error if used in PSQL queries
+const db = require('../connection');
+
 const getUserByEmail = function (email) {
-  return pool
-  .query("SELECT * FROM users WHERE email = $LIMIT 1", [email])
+  return db
+  .query("SELECT * FROM users WHERE email = $1 LIMIT 1", [email])
   .then ((result) => {
+    console.log(result.rows[0]);
     return result.rows[0];
   })
-  .catc((err) => {
+  .catch((err) => {
     console.error(err.message)
+    throw err;
   });
 };
 
 module.exports = {getUserByEmail};
+
+getUserByEmail('user7@email.com');
--- routes/account.js
+++ routes/account.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const { getUserByEmail } = require('../db/queries/getUserByEmail.js');
 const db = require("../db/connection.js");
 
 
 router.get('/', (req, res) => {
-  const user = req.session.user;
-  res.render('account', { user });
+  const email = 'user7@email.com';
+  getUserByEmail(email)
+    .then((user) => {
+      // const user = req.session.user;
+      return res.render('account', {user});
+    });
 });
 
 module.exports = router;
