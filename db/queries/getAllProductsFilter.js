6f9b763d47a2180ca24937aedaec541c0ead5f4a
Paul Formby <paulformby@icloud.com>
2024-01-11 01:11:18 +0000

    ADD, filters by categegory for browse page

--- db/queries/getAllProductsFilter.js
+++ db/queries/getAllProductsFilter.js
@@ -1,84 +1,79 @@
-const getAllProducts = function (options, limit = 10) {
-  // Initialize an array to store parameter values for the SQL query.
+const getAllProductsFilter = function (db, options = {}, limit = 10) {
   const queryParams = [];
-  // Initialize the main part of the SQL query, including the SELECT statement and the JOIN clause.
   let queryString = `
-    SELECT products.*,
+    SELECT products.*
     FROM products
     LEFT JOIN categories ON products.category_id = categories.id
+  `;
+
+  // Filter products by category
+  if (options.category) {
+    queryParams.push(`%${options.category}%`);
+    queryString += ` WHERE categories.name LIKE $${queryParams.length} `;
+  }
+
+  queryParams.push(limit);
+
+  queryString += `
     GROUP BY products.id
     ORDER BY products.date_posted
-    LIMIT $1;
-  `;
-
-  // CONDITION, filter products by category.
-  if (options.category) {
-    // Push category value with wildcard matching to queryParams array
-    queryParams.push(`%${options.category}%`);
-    // Add "AND" or "WHERE" clause based on whether other conditions have been added
-    if (queryParams.length > 1) {
-      queryString += " AND";
-    } else {
-      queryString += " WHERE";
-    }
-    queryString += ` category LIKE $${queryParams.length} `;
-  }
-
-  // CONDITION, filter products by price range.
-  if (options.minimum_price && options.maximum_price) {
-    // Push minimum and maximum price values (converted to cents) to queryParams array
-    queryParams.push(options.minimum_price * 100, options.maximum_price * 100);
-    // Add "AND" or "WHERE" clause based on whether other conditions have been added
-    if (queryParams.length > 2) {
-      queryString += " AND";
-    } else {
-      queryString += " WHERE";
-    }
-    queryString += ` price BETWEEN $${queryParams.length - 1} AND $${
-      queryParams.length
-    } `;
-  } else if (options.minimum_price) {
-    // Push minimum price value (converted to cents) to queryParams array
-    queryParams.push(options.minimum_price * 100);
-    // Add "AND" or "WHERE" clause based on whether other conditions have been added
-    if (queryParams.length > 1) {
-      queryString += " AND";
-    } else {
-      queryString += " WHERE";
-    }
-    queryString += ` price >= $${queryParams.length} `;
-  } else if (options.maximum_price) {
-    // Push maximum price value (converted to cents) to queryParams array
-    queryParams.push(options.maximum_price * 100);
-    // Add "AND" or "WHERE" clause based on whether other conditions have been added
-    if (queryParams.length > 1) {
-      queryString += " AND";
-    } else {
-      queryString += " WHERE";
-    }
-    queryString += ` price <= $${queryParams.length} `;
-  }
-
-  // CONDITION, filter products by quantity.
-  if (options.quantity) {
-    // Push category value with wildcard matching to queryParams array
-    queryParams.push(`%${options.quantity}%`);
-    // Add "AND" or "WHERE" clause based on whether other conditions have been added
-    if (queryParams.length > 1) {
-      queryString += " AND";
-    } else {
-      queryString += " WHERE";
-    }
-    queryString += ` quantity LIKE $${queryParams.length} `;
-  }
-
-  // Add ORDER BY and LIMIT clauses to complete the query.
-  queryParams.push(limit);
-  queryString += `
-    ORDER BY price
     LIMIT $${queryParams.length};
   `;
+console.log(`queryString:`, queryString);
+console.log(`queryParams:`, queryParams);
 
-  // Execute the query using the pool object and return the result rows.
-  return pool.query(queryString, queryParams).then((res) => res.rows);
+  return db.query(queryString, queryParams)
+    .then((res) => res.rows);
 };
+
+module.exports = { getAllProductsFilter };
+
+
+  // // CONDITION, filter products by price range.
+  // if (options.minimum_price && options.maximum_price) {
+  //   // Push minimum and maximum price values (converted to cents) to queryParams array
+  //   queryParams.push(options.minimum_price * 100, options.maximum_price * 100);
+  //   // Add "AND" or "WHERE" clause based on whether other conditions have been added
+  //   if (queryParams.length > 2) {
+  //     queryString += " AND";
+  //   } else {
+  //     queryString += " WHERE";
+  //   }
+  //   queryString += ` price BETWEEN $${queryParams.length - 1} AND $${
+  //     queryParams.length
+  //   } `;
+  // } else if (options.minimum_price) {
+  //   // Push minimum price value (converted to cents) to queryParams array
+  //   queryParams.push(options.minimum_price * 100);
+  //   // Add "AND" or "WHERE" clause based on whether other conditions have been added
+  //   if (queryParams.length > 1) {
+  //     queryString += " AND";
+  //   } else {
+  //     queryString += " WHERE";
+  //   }
+  //   queryString += ` price >= $${queryParams.length} `;
+  // } else if (options.maximum_price) {
+  //   // Push maximum price value (converted to cents) to queryParams array
+  //   queryParams.push(options.maximum_price * 100);
+  //   // Add "AND" or "WHERE" clause based on whether other conditions have been added
+  //   if (queryParams.length > 1) {
+  //     queryString += " AND";
+  //   } else {
+  //     queryString += " WHERE";
+  //   }
+  //   queryString += ` price <= $${queryParams.length} `;
+  // }
+
+  // // CONDITION, filter products by quantity.
+  // if (options.quantity) {
+  //   // Push category value with wildcard matching to queryParams array
+  //   queryParams.push(`%${options.quantity}%`);
+  //   // Add "AND" or "WHERE" clause based on whether other conditions have been added
+  //   if (queryParams.length > 1) {
+  //     queryString += " AND";
+  //   } else {
+  //     queryString += " WHERE";
+  //   }
+  //   queryString += ` quantity LIKE $${queryParams.length} `;
+  // }
+
--- routes/browse.js
+++ routes/browse.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getAllProducts } = require("../db/queries/getAllProducts.js");
+const { getAllProductsFilter } = require("../db/queries/getAllProductsFilter.js");
 const db = require("../db/connection.js");
 
 router.get("/", (req, res) => {
-  getAllProducts(db).then((data) => {
+  getAllProductsFilter(db).then((data) => {
     const user = req.session.user;
     console.log(data);
     res.render("browse", {data, user});
@@ -12,5 +12,20 @@ router.get("/", (req, res) => {
 
 
 });
+
+// use below when add in min/max price and qty.
+// /:minimum/:maximum
+router.get("/:filter", (req, res) => {
+  console.log(`req.params:`, req.params);
+  const options = {category: req.params.filter};
+  getAllProductsFilter(db, options).then((data) => {
+    const user = req.session.user;
+    console.log(data);
+    res.render("browse", {data, user});
+  });
+
+
+});
+
 
 module.exports = router;
