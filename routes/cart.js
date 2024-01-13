a70b2d43274060b35c9530ca3c5159ff95b42f2a
Kershia1 <insatiable.meerkat@gmail.com>
2024-01-13 00:55:39 +0000

    FIX items add to cart, display is not correct for the product image

--- routes/cart.js
+++ routes/cart.js
@@ -4,16 +4,20 @@ const db = require("../db/connection.js");
 
 router.get("/", (req, res) => {
   console.log("req.session.user", req.session.user);
-  const user = req.session.user.id;
+  const user = req.session.user;
   const query = `
   SELECT products.*, carts.quantity
   FROM carts
   JOIN products ON carts.product_id = products.id
   WHERE carts.user_id = $1
   `;
+const values = [user.id];
+
   db.query(query, [user.id])
     .then(result => {
+      console.log(result);
       const cartItems = result.rows;
+      console.log(cartItems);
       res.render("cart", { user, cartItems });
     })
     .catch(err => res.status(500).send(err));
@@ -23,23 +27,20 @@ router.get("/", (req, res) => {
 //note to self table is named carts not cart
 
 router.post("/", (req, res) => {
-
-  const { product_id, quanitity, action } = req.body;
-  //const user = req.session.user.id;
+  const { itemId: product_id, quantity, action } = req.body;
   const user = req.session.user;
 
   //if the product is not in the cart, insert the product and quantity
-  if (action === "add") {
+  if (action === "add-cart") {
   const query = `
-INSERT INTO CARTS (user_id, product_id, quanitiy)
+INSERT INTO CARTS (user_id, product_id, quantity)
 SELECT $1, $2, $3
 WHERE NOT EXISTS (
   SELECT * FROM carts WHERE user_id = $1 AND product_id = $2
 )
 `;
-// const values = [user.id, product_id, quanitity];
-  const values = [user, product_id, quanitity];
 
+const values = [user.id, product_id, quantity];
   db.query(query, values)
     .then(() => res.redirect("/cart"))
     .catch(err => res.status(500).send(err));
@@ -47,8 +48,8 @@ WHERE NOT EXISTS (
     const query = `
     DELETE FROM carts
     WHERE user_id = $1 AND product_id = $2`;
-    // const values = [user.id, product_id];
-    const values = [user, product_id];
+
+    const values = [user.id, product_id];
 
     db.query(query, values)
       .then(() => res.redirect("/cart"))
